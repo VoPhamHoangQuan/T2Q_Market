d7b8c5c9fb3fce2cf59937ac026cde71bdafc728
PC <vophamhoangquan140620@gmail.com>
2021-10-31 17:34:12 +0700

    init fix

---
+++ src/data.js
@@ -0,0 +1,90 @@
+const data = {
+    products: [
+        {
+            productid: 0,
+            cateid: 0,
+            companyid: 0,
+            price: 120,
+            amount: 2,
+            name: 'product 01',
+            decription: 'description 01',
+            origin: 'origin 01',
+            image: '/image/product01.jpg',
+            discount: null,
+            IsDelete: null
+
+        },
+        {
+            productid: 1,
+            cateid: 0,
+            companyid: 0,
+            price: 120,
+            amount: 2,
+            name: 'product 02',
+            decription: 'description 02',
+            origin: 'origin 02',
+            image: '/image/product01.jpg',
+            discount: null,
+            IsDelete: null
+
+        },
+        {
+            productid: 2,
+            cateid: 0,
+            companyid: 0,
+            price: 120,
+            amount: 2,
+            name: 'product 03',
+            decription: 'description 01',
+            origin: 'origin 01',
+            image: '/image/product01.jpg',
+            discount: null,
+            IsDelete: null
+
+        },
+        {
+            productid: 3,
+            cateid: 0,
+            companyid: 0,
+            price: 120,
+            amount: 2,
+            name: 'product 04',
+            decription: 'description 01',
+            origin: 'origin 01',
+            image: '/image/product01.jpg',
+            discount: null,
+            IsDelete: null
+
+        },
+        {
+            productid: 4,
+            cateid: 0,
+            companyid: 0,
+            price: 120,
+            amount: 2,
+            name: 'product 05',
+            decription: 'description 01',
+            origin: 'origin 01',
+            image: '/image/product01.jpg',
+            discount: null,
+            IsDelete: null
+
+        },
+        {
+            productid: 5,
+            cateid: 0,
+            companyid: 0,
+            price: 120,
+            amount: 2,
+            name: 'product 06',
+            decription: 'description 01',
+            origin: 'origin 01',
+            image: '/image/product01.jpg',
+            discount: null,
+            IsDelete: null
+
+        },
+    ]
+}
+
+export default data
